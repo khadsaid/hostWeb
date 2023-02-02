ee3d73ef9b81a4221c8aee2c280025c524d84985
unknown <khadijasuleiman7027@gmail.com>
2023-02-02 01:12:13 -0800

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashComponent } from './dash/dash.component';
+import { IndexComponent } from './index/index.component';
+import { LoginComponent } from './login/login.component';
+import { TableComponent } from './table/table.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'',component:LoginComponent
+},
+{
+  path:'index',component:IndexComponent
+},{
+  path:'dash',component:DashComponent
+},
+{
+  path:'table',component:TableComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { IndexComponent } from './index/index.component';
+import { DashComponent } from './dash/dash.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    IndexComponent,
+    DashComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
